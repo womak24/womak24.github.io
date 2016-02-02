23a6ebb2427ba2434c9634259e75082ebf44aa05
Immanuel P <hackberd@gmail.com>
2016-02-03 00:46:20 +0100

    Text align in bild (subsides)

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,3 @@
-// http://codepen.io/Guilh/pen/JLKbn
-// https://teamtreehouse.com/community/forum-tip-create-a-sticky-navigation-with-css-and-jquery-2
-
-var mn = $(".navbar");
-mns = "main-nav-scrolled";
-hdr = $('header').height();
-
-$(window).scroll(function () {
-    if ($(this).scrollTop() > hdr) {
-        mn.addClass(mns);
-    } else {
-        mn.removeClass(mns);
-    }
-});
 
 $(document).ready(function () {
     $('.slider-content').slick({
